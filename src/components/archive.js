70512f33599e9ffa5730e15e37a7c85adf760e7f
Daniel Adler <weston.adler@gmail.com>
2019-07-22 19:47:37 -0700

    lessons 9,10: setting up archive component and filtered/sorted query

---
+++ src/components/archive.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+
+const POST_ARCHIVE_QUERY = graphql`
+  query BlogPostArchive	 {
+    allMarkdownRemark(limit: 5, sort: {
+    	order:DESC,
+      fields:[frontmatter___date]
+    }) {
+      edges {
+        node {
+  				frontmatter {
+            title
+            slug
+            date(formatString: "MMMM DD, YYYY")
+          }
+        }
+      }
+    }
+  }
+`
+
+const Archive = () => {
+  const { allMarkdownRemark } =  useStaticQuery(POST_ARCHIVE_QUERY)
+
+  return (
+    <>
+      <h3>Archive</h3>
+      <ul>
+        {allMarkdownRemark.edges.map(edge => (
+          <li key={edge.node.frontmatter.slug}>
+            <Link to={`/posts${edge.node.frontmatter.slug}`}>
+              {edge.node.frontmatter.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default Archive
